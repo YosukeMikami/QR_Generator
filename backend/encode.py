45083cc21a0badee0c14593a2de2397b91377180
YosukeMikami <ymikami@is.s.u-tokyo.ac.jp>
2025-06-17 22:33:10 +0900

    modify Segment class

--- backend/encode.py
+++ backend/encode.py
@@ -5,19 +5,105 @@ import errorcorrectiondata as ecd
 from errorcode import CalculateRemainder
 from data import Mode
 
-
 class Segment:
-    def __init__(self, mode):
+    def __init__(self, mode=Mode.kEightBitByte):
         self.mode = mode
-        self.code = []
-        self.len = 0
+        self.message = ""
 
-    def __repr__(self):
-        return f"mode:{self.mode}, len:{self.len},\n{self.code}"
+    def Encode(self):
+        if self.mode == Mode.kNumber:
+            return self.EncodeNumber()
+        elif self.mode == Mode.kAlphaNum:
+            return self.EncodeAlphaNum()
+        elif self.mode == Mode.kEightBitByte:
+            return self.Encode8BitByte()
+        else:
+            return self.EncodeKanji()
+
+    def BitToList(bit, length):
+        return [
+            True if (bit >> (length - i - 1)) & 1 == 1\
+            else False\
+            for i in range(length)
+        ]
+
+    def EncodeNumber(self):
+        out = []
+        # 3文字ごとに分割
+        for i in range(0, len(self.message), 3):
+            part = self.message[i : i + 3]
+            try:
+                if len(part) == 1:
+                        out.extend(Segment.BitToList(int(part), 4))
+                elif len(part) == 2:
+                    # 2文字を7bitに圧縮
+                    out.extend(Segment.BitToList(int(part), 7))
+                else:
+                    # 3文字を10bitに圧縮
+                    out.extend(Segment.BitToList(int(part), 10))
+            except ValueError:
+                raise ValueError("Input string must contain only numbers.")
+        return out
 
 
-def BitToList(bit, length):
-    return [True if (bit >> (length - i - 1)) & 1 == 1 else False for i in range(length)]
+    def EncodeAlphaNum(self):
+        def CodeOfAlphaNum(char):
+            alpha_num_symbols = {" ": 36, "$": 37, "%": 38, "*": 39, "+": 40, "-": 41, ".": 42, "/": 43, ":": 44}
+            if ord("0") <= ord(char) <= ord("9"):
+                return int(char)
+            if ord("A") <= ord(char) <= ord("Z"):
+                return 10 + ord(char) - ord("A")
+            if char in alpha_num_symbols:
+                return alpha_num_symbols[char]
+            raise ValueError("Input string must contain only numbers, uppercase alphabetical characters, and certain symbols.")
+
+        out = []
+        # 2文字ごとに分割
+        for i in range(0, len(self.message), 2):
+            part = self.message[i : i + 2]
+            try:
+                if len(part) == 1:
+                    out.extend(Segment.BitToList(CodeOfAlphaNum(part), 6))
+                else:
+                    upper_code = CodeOfAlphaNum(part[0])
+                    lower_code = CodeOfAlphaNum(part[1])
+                    # 2文字を11bitに圧縮
+                    out.extend(Segment.BitToList(upper_code * 45 + lower_code, 11))
+            except ValueError as e:
+                raise e
+        return out
+
+    def Encode8BitByte(self):
+        def CodeofChar(char):
+            code = int(char.encode("cp932").hex(), 16)
+            if (code >> 8) == 0:
+                return Segment.BitToList(code, 8)
+            else:
+                return Segment.BitToList(code, 16)
+
+        out = []
+        for char in self.message:
+            out.extend(CodeofChar(char))
+        return out
+
+    def EncodeKanji(self):
+        def CodeOfChar(char):
+            code = int(char.encode("cp932").hex(), 16)
+            if 0x8140 <= code <= 0x9ffc:
+                code -= 0x8140
+            elif 0xe040 <= code <= 0xebbf:
+                code -= 0xc140
+            else:
+                raise ValueError("Input character must be Kanji")
+            upper_byte = (code >> 8) & 0xff
+            lower_byte = code & 0xff
+            res = upper_byte * 0xc0 + lower_byte
+            return Segment.BitToList(res, 13)
+
+        out = []
+        for char in self.message:
+            out.extend(CodeOfChar(char))
+        return out
 
 
 def ListToBit(list, length):
@@ -27,89 +113,54 @@ def ListToBit(list, length):
     return word
 
 
-def EncodeNumber(string):
-    num = int(string)
-    if len(string) == 1:
-        return BitToList(num, 4)
-    if len(string) == 2:
-        return BitToList(num, 7)
-    return BitToList(num, 10)
 
-
-alpha_num_symbols = {" ": 36, "$": 37, "%": 38, "*": 39, "+": 40, "-": 41, ".": 42, "/": 43, ":": 44}
-
-
-def CodeOfAlphaNum(char):
-    if ord("0") <= ord(char) <= ord("9"):
-        return ord(char)
-    if ord("A") <= ord(char) <= ord("Z"):
-        return 10 + ord(char) - ord("A")
-    return alpha_num_symbols[char]
-
-
-def EncodeAlphaNum(string):
-    if len(string) == 1:
-        return BitToList(CodeOfAlphaNum(string), 6)
-    else:
-        upper_code = CodeOfAlphaNum(string[0])
-        lower_code = CodeOfAlphaNum(string[1])
-        return BitToList(upper_code * 45 + lower_code, 11)
-
-
-def Encode8BitByte(char):
-    code = int(char.encode("cp932").hex(), 16)
-    if (code >> 8) == 0:
-        return BitToList(code, 8)
-    else:
-        return BitToList(code, 16)
-
-
-def EncodeKanji(char):
-    code = int(char.encode("cp932").hex(), 16)
-    if 0x8140 <= code <= 0x9ffc:
-        code -= 0x8140
-    else:
-        code -= 0xc140
-    upper_byte = (code >> 8) & 0xff
-    lower_byte = code & 0xff
-    res = upper_byte * 0xc0 + lower_byte
-    return BitToList(res, 13)
-
-
-# def MinModeLen(string):
-
+# def EncodeKanji(char):
+#     code = int(char.encode("cp932").hex(), 16)
+#     if 0x8140 <= code <= 0x9ffc:
+#         code -= 0x8140
+#     else:
+#         code -= 0xc140
+#     upper_byte = (code >> 8) & 0xff
+#     lower_byte = code & 0xff
+#     res = upper_byte * 0xc0 + lower_byte
+#     return BitToList(res, 13)
 
 # this function needs to be refactored.
-def Encode(string, error_correction_level):
-    segment = Segment(Mode.kEightBitByte)
-    for char in string:
-        segment.code.extend(Encode8BitByte(char))
-    version = DecideVersion(4 + data.LenIndicatorLen(1, Mode.kEightBitByte) + len(segment.code), error_correction_level)
-    if version <= 9:
-        segment.len = len(segment.code) // 8
-        indicater_len = data.LenIndicatorLen(1, Mode.kEightBitByte)
-        data_code = BitToList(segment.mode, 4)
-        data_code.extend(BitToList(segment.len, indicater_len))
-        data_code.extend(segment.code)
-        return data_code, version
-    version = DecideVersion(4 + data.LenIndicatorLen(10, Mode.kEightBitByte) + len(segment.code), error_correction_level)
-    if version <= 26:
-        segment.len = len(segment.code) // 8
-        indicater_len = data.LenIndicatorLen(10, Mode.kEightBitByte)
-        data_code = BitToList(segment.mode, 4)
-        data_code.extend(BitToList(segment.len, indicater_len))
-        data_code.extend(segment.code)
-        return data_code, version
-    version = DecideVersion(4 + data.LenIndicatorLen(27, Mode.kEightBitByte) + len(segment.code), error_correction_level)
-    if version <= 40:
-        segment.len = len(segment.code) // 8
-        indicater_len = data.LenIndicatorLen(27, Mode.kEightBitByte)
-        data_code = BitToList(segment.mode, 4)
-        data_code.extend(BitToList(segment.len, indicater_len))
-        data_code.extend(segment.code)
-        return data_code, version
-    print("The input string is too long to encode!")
-    sys.exit(1)
+def Encode(message, error_correction_level):
+    segment = Segment(Mode.kKanji)
+    for char in message:
+        segment.message += char
+    code = segment.Encode()
+    return code
+    # segment = Segment(Mode.kEightBitByte)
+    # for char in string:
+    #     segment.code.extend(Encode8BitByte(char))
+    # version = DecideVersion(4 + data.LenIndicatorLen(1, Mode.kEightBitByte) + len(segment.code), error_correction_level)
+    # if version <= 9:
+    #     segment.len = len(segment.code) // 8
+    #     indicater_len = data.LenIndicatorLen(1, Mode.kEightBitByte)
+    #     data_code = BitToList(segment.mode, 4)
+    #     data_code.extend(BitToList(segment.len, indicater_len))
+    #     data_code.extend(segment.code)
+    #     return data_code, version
+    # version = DecideVersion(4 + data.LenIndicatorLen(10, Mode.kEightBitByte) + len(segment.code), error_correction_level)
+    # if version <= 26:
+    #     segment.len = len(segment.code) // 8
+    #     indicater_len = data.LenIndicatorLen(10, Mode.kEightBitByte)
+    #     data_code = BitToList(segment.mode, 4)
+    #     data_code.extend(BitToList(segment.len, indicater_len))
+    #     data_code.extend(segment.code)
+    #     return data_code, version
+    # version = DecideVersion(4 + data.LenIndicatorLen(27, Mode.kEightBitByte) + len(segment.code), error_correction_level)
+    # if version <= 40:
+    #     segment.len = len(segment.code) // 8
+    #     indicater_len = data.LenIndicatorLen(27, Mode.kEightBitByte)
+    #     data_code = BitToList(segment.mode, 4)
+    #     data_code.extend(BitToList(segment.len, indicater_len))
+    #     data_code.extend(segment.code)
+    #     return data_code, version
+    # print("The input string is too long to encode!")
+    # sys.exit(1)
 
 
 def DivideCodePer8Bit(code):
@@ -207,3 +258,12 @@ def EncodeVersionInfo(version):
     code.extend(error_code)
     res = ListToBit(code, code.shape[0])
     return res
+
+if __name__ == "__main__":
+    code = Encode("茗", None)
+    s = ""
+    for i, b in enumerate(code):
+        s += "1" if b else "0"
+        # if i % 8 == 7:
+        #     s += " "
+    print(s)
