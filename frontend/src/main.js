2690c297c06e2fe6ccdeb1795e25b4c288b1aaee
YosukeMikami <ymikami@is.s.u-tokyo.ac.jp>
2025-06-21 20:08:54 +0900

    add some functions

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,22 +1,55 @@
-import './style.css';
-
 async function ButtonClick() {
+  const text = document.getElementById("input_text").value;
+  if (text == "") {
+    return;
+  }
+  const error_correction_level = document.getElementById("error_level_select").value;
+  const format = document.getElementById("format_select").value;
+  const size = document.getElementById("size_input").value;
   const res = await fetch("http://localhost:8000/generate", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify(
       {
-        "text": "hello",
-        "error_correction_level": "M",
-        "format": "png",
-        "size": 10
+        "text": text,
+        "error_correction_level": error_correction_level,
+        "format": format,
+        "size": size
       }
     )
   });
   const data = await res.blob();
   const url = URL.createObjectURL(data);
   document.getElementById("qr_image").src = url;
+  let a = document.getElementById("download_a");
+  a.href = url;
+  const d = new Date();
+  a.download = `qr_code_${d.getTime()}.${format}`;
+  a.classList.add("can_download");
 }
 
-let button = document.getElementById("button");
+function CanGenerateCheck() {
+  const text = document.getElementById("input_text").value;
+  const size_input = document.getElementById("size_input");
+  let generate_button = document.getElementById("generate_button");
+  if (
+    text != "" &&
+    !size_input.validity.rangeOverflow &&
+    !size_input.validity.rangeUnderflow
+  ) {
+    generate_button.classList.add("can_generate");
+    generate_button.classList.remove("cannot_generate");
+  } else {
+    generate_button.classList.add("cannot_generate");
+    generate_button.classList.remove("can_generate");
+  }
+}
+
+let button = document.getElementById("generate_button");
 button.addEventListener("click", ButtonClick);
+
+let textarea = document.getElementById("input_text");
+textarea.addEventListener("input", CanGenerateCheck);
+
+let size_input = document.getElementById("size_input");
+size_input.addEventListener("input", CanGenerateCheck);
