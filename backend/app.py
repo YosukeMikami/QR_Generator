aebf2f506c0d040d20778281076a5d6ce45c531f
YosukeMikami <ymikami@is.s.u-tokyo.ac.jp>
2025-06-30 20:43:58 +0900

    add ping of HEAD

--- backend/app.py
+++ backend/app.py
@@ -24,9 +24,12 @@ class Request(BaseModel):
     size: int
     dpi: int
 
+@app.api_route("/ping", methods=["GET", "HEAD"])
+async def ping():
+    return "OK"
 
 @app.post("/generate/")
-def generate(request: Request):
+async def generate(request: Request):
     try:
         buf = qrgenerate.main(
             request.text,
